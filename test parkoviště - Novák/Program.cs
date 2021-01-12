eb7f758689b3eea737d42b9995afa9980bec558b
Miroslav Novák <u19.417@pslib.cloud>
2021-01-12 08:13:19 +0100

    Přidejte soubory projektu.

---
+++ test parkoviště - Novák/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace test_parkoviště___Novák
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
